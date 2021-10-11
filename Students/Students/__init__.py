f63c75b8141e93600e4c418c93c6a2f70ea28949
Morana <angie77brennan@gmail.com>
2021-10-11 19:21:12 +0100

    Part 1 - creating student id's, modules and grades

---
+++ Students/Students/__init__.py
@@ -0,0 +1,24 @@
+"""
+#
+# File        : __init__.py.py
+# Created     ：11/10/2021 19:04
+# Author      ：Angela Brennan
+# Version     ：v1.0.0
+# Licencing   : (C) 2021 Angela Brennan, LYIT
+#            Available under GNU Public License (GPL)
+# Description ：
+#
+"""
+
+if __name__ == '__main__':
+    '''
+      Main method of application
+
+      Linear programming only presented here wrt demo of lists
+
+      Parameters:
+        none
+
+      Returns:
+        none
+    '''
---
+++ Students/Students/main.py
@@ -0,0 +1,32 @@
+"""
+#
+# File        : main.py
+# Created     ：11/10/2021 19:04
+# Author      ：Angela Brennan
+# Version     ：v1.0.0
+# Licencing   : (C) 2021 Angela Brennan, LYIT
+#            Available under GNU Public License (GPL)
+# Description ：
+#
+"""
+
+if __name__ == '__main__':
+    '''
+      Main method of application
+
+      Linear programming only presented here wrt demo of lists
+
+      Parameters:
+        none
+
+      Returns:
+        none
+    '''
+
+    student_id = [('L134532', 'L295320')]
+    modules = ['Java_ooprogramming', 'Python_Scripting']
+    Java_grades = {'L134532': '70', 'L295320': '59'}
+    Python_grades = {'L134532': '64', 'L295320': '82'}
+
+    print(Python_grades)
+    print(Java_grades)
