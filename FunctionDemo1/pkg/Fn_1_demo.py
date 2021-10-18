195a11df6da56812ae8242dbe0fe889038642c76
Morana <angie77brennan@gmail.com>
2021-10-18 19:48:18 +0100

    Sample text file

---
+++ FunctionDemo1/pkg/Fn_1_demo.py
@@ -0,0 +1,39 @@
+"""
+#
+# File        : Fn_1_demo.py
+# Created     ：18/10/2021 17:40
+# Author      ：Angela Brennan
+# Version     ：v1.0.0
+# Licencing   : (C) 2021 Angela Brennan, LYIT
+#            Available under GNU Public License (GPL)
+# Description ：
+#
+"""
+
+if __name__ == '__main__':
+
+    '''
+    Main method of application
+
+      Linear programming only presented here wrt demo of functions
+
+      Parameters:
+        none
+
+      Returns:
+        none  
+        
+    '''
+
+
+    def print_hi(stu_name):
+        """..."""
+        print(f"Hello {stu_name}!")  # Formatted string example
+
+    print("I print first")  # print line as encountered first
+
+    if __name__ == '__main__':
+            '''...'''
+            print_hi("Dian")
+
+        print("I print anyway")
---
+++ file_handling_demo_1/pkg/file_eg_1.py
@@ -0,0 +1,24 @@
+"""
+#
+# File        : file_eg_1.py
+# Created     ：18/10/2021 19:44
+# Author      ：Angela Brennan
+# Version     ：v1.0.0
+# Licencing   : (C) 2021 Angela Brennan, LYIT
+#            Available under GNU Public License (GPL)
+# Description ：
+#
+"""
+
+if __name__ == '__main__':
+    '''
+      Main method of application
+
+      File Processing
+      Parameters:
+        none
+
+      Returns:
+        none
+    '''
+def file_
